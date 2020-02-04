c4249444b8648f684b17b3533059314d25f1ccbe
Marlon <luciousmc@icloud.com>
2020-02-03 23:29:11 -0800

    foreach with associative arrays

--- exercises/php_for_loop/index.php
+++ exercises/php_for_loop/index.php
@@ -7,3 +7,22 @@
     echo "<div>" . $people[$peopleI] . "</div>";
   }
 ?>
+
+<?php 
+  $film = [
+    'title' => 'The Shawshank Redemption',
+    'year' => 1994,
+    'director' => 'Frank Darabont',
+    'cast' => [
+      'Andy Dufresnne' => 'Tim Robbins',
+      'Ellis "Red" Redding' => 'Morgan Freeman',
+      'Warden Norton' => 'Bob Gunton'
+    ]
+  ];
+  $sentence = "{$film['title']} was made in {$film['year']} and directed by {$film['director']}.<br>
+  Cast: ";
+  foreach ( $film['cast'] as $role => $actor) {
+    $sentence .= "<br>{$role} - {$actor}";
+  }
+  echo "<pre>$sentence</pre>";
+?>
