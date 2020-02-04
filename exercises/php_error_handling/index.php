829b5dd0faae5fd5ecaa96bfe14d157abdd035e7
Marlon <luciousmc@icloud.com>
2020-02-03 21:33:48 -0800

    random numbers throw exception on even

--- exercises/php_error_handling/index.php
+++ exercises/php_error_handling/index.php
@@ -3,11 +3,17 @@
   function error_handler( $error ) {
     print "<strong>Monas Error:</strong> " . $error->getMessage();
   }
-  // set_error_handler('error_handler');
-
   set_exception_handler('error_handler');
 
   // throw new Exception ('I want an error');
 
-  doStuff('take out the trash');
+  // doStuff('take out the trash');
+
+  $randomNumber = rand(1, 10);
+
+  if ($randomNumber % 2 === 1) {
+    echo "The number was $randomNumber";
+  } else {
+    throw new Exception('Exception: The number was even');
+  }
 ?>
