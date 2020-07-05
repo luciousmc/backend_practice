5c2dfb38972dfd3849c1249351a1fbb48b94d0e2
Marlon <luciousmc@icloud.com>
2020-07-04 21:10:20 -0700

    php switch completed

--- exercises/php_if_switch/index.php
+++ exercises/php_if_switch/index.php
@@ -8,3 +8,24 @@
     print "$currentTime is Even";
   }
 ?>
+
+<?php
+$dateString = date("n\-j");
+$message = "";
+
+switch($dateString) {
+  case "12-25":
+    $message = "Merry Christmas";
+  break;
+  case "1-1":
+    $message = "Happy New Year";
+  break;
+  case "2-14":
+    $message = "Happy Valentine's Day";
+  break;
+  case "7-4":
+    $message = "Fireworks! Yay!";
+  break;
+}
+print $message;
+?>
